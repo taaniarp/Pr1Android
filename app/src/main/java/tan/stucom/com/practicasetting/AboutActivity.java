ef774f9e40eb748dfa5f9205d5db8114553cf11f
taniarp <tania.ramon.prats@gmail.com>
2018-10-24 17:06:07 +0200

    Update with some random location (in my case Port Aventura)

--- app/src/main/java/tan/stucom/com/practicasetting/AboutActivity.java
+++ app/src/main/java/tan/stucom/com/practicasetting/AboutActivity.java
@@ -1,7 +1,10 @@
 package tan.stucom.com.practicasetting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button btnMaps = findViewById(R.id.btnMaps);
+        btnMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
